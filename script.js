076fe062350d595fe540fcf954e2d136575a8cc0
iwatahotaka <115777656+iwatahotaka@users.noreply.github.com>
2022-10-25 22:23:34 +0800

    ファイル移動完了

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById('button1').onclick = function() {
+    document.getElementById('text').value += 1;
+}
+document.getElementById('button2').onclick = function() {
+    document.getElementById('text').value += 2;
+}
+document.getElementById('button3').onclick = function() {
+    document.getElementById('text').value += 3;
+}
+document.getElementById('button4').onclick = function() {
+    document.getElementById('text').value += 4;
+}
+document.getElementById('button5').onclick = function() {
+    document.getElementById('text').value += 5;
+}
+document.getElementById('button6').onclick = function() {
+    document.getElementById('text').value += 6;
+}
+document.getElementById('button7').onclick = function() {
+    document.getElementById('text').value += 7;
+}
+document.getElementById('button8').onclick = function() {
+    document.getElementById('text').value += 8;
+}
+document.getElementById('button9').onclick = function() {
+    document.getElementById('text').value += 9;
+}
+document.getElementById('CRL').onclick = function() {
+    document.getElementById('text').value = " ";
+}
